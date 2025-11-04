725336f1f599248f31a91d3faa5933be7a9c1b93
Sauli616 <albinoneekeri23@gmail.com>
2025-11-04 10:08:55 +0200

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,3 +1,4 @@
+
 namespace Sauli616_Executor
 {
     partial class Form1
@@ -15,184 +16,194 @@ namespace Sauli616_Executor
 
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();      // Execute
-            this.button2 = new System.Windows.Forms.Button();      // Inject
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();      // Close
-            this.button4 = new System.Windows.Forms.Button();      // Minimize
-            this.button5 = new System.Windows.Forms.Button();      // Logo
-            this.button6 = new System.Windows.Forms.Button();      // Open File
-            this.button7 = new System.Windows.Forms.Button();      // Save File
-            this.button8 = new System.Windows.Forms.Button();      // Infinite Yield
-
-            this.SuspendLayout();
-
-            // button1 - Execute
-            this.button1.BackColor = System.Drawing.Color.FromArgb(34, 34, 34);
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(50, 50, 50);
-            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(60, 60, 60);
-            this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.button1.Location = new System.Drawing.Point(362, 181);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(57, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Execute";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-
-            // button2 - Inject
-            this.button2.BackColor = System.Drawing.Color.FromArgb(34, 34, 34);
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(50, 50, 50);
-            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(60, 60, 60);
-            this.button2.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.button2.Location = new System.Drawing.Point(299, 181);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(57, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Inject";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-
+            button1 = new Button();
+            button2 = new Button();
+            richTextBox1 = new RichTextBox();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            label1 = new Label();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(34, 34, 34);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(60, 60, 60);
+            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(50, 50, 50);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.WhiteSmoke;
+            button1.Location = new Point(362, 181);
+            button1.Name = "button1";
+            button1.Size = new Size(57, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Execute";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(34, 34, 34);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseDownBackColor = Color.FromArgb(60, 60, 60);
+            button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(50, 50, 50);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.WhiteSmoke;
+            button2.Location = new Point(299, 181);
+            button2.Name = "button2";
+            button2.Size = new Size(57, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Inject";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
             // richTextBox1
-            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(34, 34, 34);
-            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox1.ForeColor = System.Drawing.Color.LightGray;
-            this.richTextBox1.Location = new System.Drawing.Point(12, 31);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(407, 144);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
-
+            // 
+            richTextBox1.BackColor = Color.FromArgb(34, 34, 34);
+            richTextBox1.BorderStyle = BorderStyle.None;
+            richTextBox1.ForeColor = Color.LightGray;
+            richTextBox1.Location = new Point(12, 31);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(407, 144);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
             // label1
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label1.Location = new System.Drawing.Point(36, 7);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 15);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Sauli616";
-
-            // button3 - Close
-            this.button3.BackColor = System.Drawing.Color.FromArgb(28, 28, 28);
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(70, 70, 70);
-            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(80, 80, 80);
-            this.button3.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.button3.Location = new System.Drawing.Point(406, 4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(22, 21);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "X";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-
-            // button4 - Minimize
-            this.button4.BackColor = System.Drawing.Color.FromArgb(28, 28, 28);
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(70, 70, 70);
-            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(80, 80, 80);
-            this.button4.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.button4.Location = new System.Drawing.Point(378, 4);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(22, 21);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "-";
-            this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-
-            // button5 - Logo
-            this.button5.BackgroundImage = global::Sauli616_Executor.Properties.Resources.Nemhoo;
-            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button5.Enabled = false;
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.FlatAppearance.BorderSize = 0;
-            this.button5.Location = new System.Drawing.Point(2, 3);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(28, 22);
-            this.button5.TabIndex = 6;
-            this.button5.UseVisualStyleBackColor = true;
-
-            // button6 - Open File
-            this.button6.BackColor = System.Drawing.Color.FromArgb(24, 24, 24);
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.FlatAppearance.BorderSize = 0;
-            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(50, 50, 50);
-            this.button6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(60, 60, 60);
-            this.button6.Font = new System.Drawing.Font("Segoe UI Emoji", 10F);
-            this.button6.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.button6.Location = new System.Drawing.Point(12, 181);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(23, 23);
-            this.button6.TabIndex = 8;
-            this.button6.Text = "📂";
-            this.button6.UseVisualStyleBackColor = false;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-
-            // button7 - Save File
-            this.button7.BackColor = System.Drawing.Color.FromArgb(24, 24, 24);
-            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button7.FlatAppearance.BorderSize = 0;
-            this.button7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(50, 50, 50);
-            this.button7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(60, 60, 60);
-            this.button7.Font = new System.Drawing.Font("Segoe UI Emoji", 10F);
-            this.button7.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.button7.Location = new System.Drawing.Point(42, 181);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(23, 23);
-            this.button7.TabIndex = 9;
-            this.button7.Text = "🗃️";
-            this.button7.UseVisualStyleBackColor = false;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-
-            // button8 - Infinite Yield
-            this.button8.BackColor = System.Drawing.Color.FromArgb(34, 34, 34);
-            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button8.FlatAppearance.BorderSize = 0;
-            this.button8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(50, 50, 50);
-            this.button8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(60, 60, 60);
-            this.button8.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.button8.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold);
-            this.button8.Location = new System.Drawing.Point(71, 181);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(27, 23);
-            this.button8.TabIndex = 10;
-            this.button8.Text = "IY";
-            this.button8.UseVisualStyleBackColor = false;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
-
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.WhiteSmoke;
+            label1.Location = new Point(36, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Sauli616";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(28, 28, 28);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatAppearance.MouseDownBackColor = Color.FromArgb(80, 80, 80);
+            button3.FlatAppearance.MouseOverBackColor = Color.FromArgb(70, 70, 70);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.WhiteSmoke;
+            button3.Location = new Point(406, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(22, 21);
+            button3.TabIndex = 4;
+            button3.Text = "X";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(28, 28, 28);
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatAppearance.MouseDownBackColor = Color.FromArgb(80, 80, 80);
+            button4.FlatAppearance.MouseOverBackColor = Color.FromArgb(70, 70, 70);
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.ForeColor = Color.WhiteSmoke;
+            button4.Location = new Point(378, 4);
+            button4.Name = "button4";
+            button4.Size = new Size(22, 21);
+            button4.TabIndex = 5;
+            button4.Text = "-";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackgroundImage = Properties.Resources.Nemhoo;
+            button5.BackgroundImageLayout = ImageLayout.Stretch;
+            button5.Enabled = false;
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Location = new Point(2, 3);
+            button5.Name = "button5";
+            button5.Size = new Size(28, 22);
+            button5.TabIndex = 6;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(24, 24, 24);
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatAppearance.MouseDownBackColor = Color.FromArgb(60, 60, 60);
+            button6.FlatAppearance.MouseOverBackColor = Color.FromArgb(50, 50, 50);
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Segoe UI Emoji", 10F);
+            button6.ForeColor = Color.WhiteSmoke;
+            button6.Location = new Point(12, 181);
+            button6.Name = "button6";
+            button6.Size = new Size(23, 23);
+            button6.TabIndex = 8;
+            button6.Text = "📂";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(24, 24, 24);
+            button7.FlatAppearance.BorderSize = 0;
+            button7.FlatAppearance.MouseDownBackColor = Color.FromArgb(60, 60, 60);
+            button7.FlatAppearance.MouseOverBackColor = Color.FromArgb(50, 50, 50);
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Segoe UI Emoji", 10F);
+            button7.ForeColor = Color.WhiteSmoke;
+            button7.Location = new Point(42, 181);
+            button7.Name = "button7";
+            button7.Size = new Size(23, 23);
+            button7.TabIndex = 9;
+            button7.Text = "🗃️";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(24, 24, 24);
+            button8.FlatAppearance.BorderSize = 0;
+            button8.FlatAppearance.MouseDownBackColor = Color.FromArgb(60, 60, 60);
+            button8.FlatAppearance.MouseOverBackColor = Color.FromArgb(50, 50, 50);
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Segoe UI", 8F, FontStyle.Bold);
+            button8.ForeColor = Color.WhiteSmoke;
+            button8.Location = new Point(71, 181);
+            button8.Name = "button8";
+            button8.Size = new Size(27, 23);
+            button8.TabIndex = 10;
+            button8.Text = "IY";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
             // Form1
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(24, 24, 24);
-            this.ClientSize = new System.Drawing.Size(431, 216);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.DoubleBuffered = true;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Form1";
-            this.Opacity = 0.94D;
-            this.Text = "Sauli616";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(24, 24, 24);
+            ClientSize = new Size(431, 216);
+            Controls.Add(button8);
+            Controls.Add(button6);
+            Controls.Add(button7);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(label1);
+            Controls.Add(richTextBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Opacity = 0.94D;
+            Text = "Sauli616";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
