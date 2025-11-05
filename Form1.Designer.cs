792d2224c973fef77024c2109f503ddc29ec55fa
Sauli616 <albinoneekeri23@gmail.com>
2025-11-05 15:18:13 +0200

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,3 @@
-
 namespace Sauli616_Executor
 {
     partial class Form1
@@ -27,6 +26,7 @@ namespace Sauli616_Executor
             button6 = new Button();
             button7 = new Button();
             button8 = new Button();
+            button9 = new Button();
             SuspendLayout();
             // 
             // button1
@@ -37,11 +37,11 @@ namespace Sauli616_Executor
             button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(50, 50, 50);
             button1.FlatStyle = FlatStyle.Flat;
             button1.ForeColor = Color.WhiteSmoke;
-            button1.Location = new Point(362, 181);
+            button1.Location = new Point(422, 218);
             button1.Name = "button1";
-            button1.Size = new Size(57, 23);
+            button1.Size = new Size(68, 23);
             button1.TabIndex = 0;
-            button1.Text = "Execute";
+            button1.Text = "▶Execute";
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
@@ -53,11 +53,11 @@ namespace Sauli616_Executor
             button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(50, 50, 50);
             button2.FlatStyle = FlatStyle.Flat;
             button2.ForeColor = Color.WhiteSmoke;
-            button2.Location = new Point(299, 181);
+            button2.Location = new Point(348, 219);
             button2.Name = "button2";
-            button2.Size = new Size(57, 23);
+            button2.Size = new Size(68, 23);
             button2.TabIndex = 1;
-            button2.Text = "Inject";
+            button2.Text = "🔗Attach";
             button2.UseVisualStyleBackColor = false;
             button2.Click += button2_Click;
             // 
@@ -68,7 +68,7 @@ namespace Sauli616_Executor
             richTextBox1.ForeColor = Color.LightGray;
             richTextBox1.Location = new Point(12, 31);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(407, 144);
+            richTextBox1.Size = new Size(478, 181);
             richTextBox1.TabIndex = 2;
             richTextBox1.Text = "";
             richTextBox1.TextChanged += richTextBox1_TextChanged;
@@ -76,7 +76,7 @@ namespace Sauli616_Executor
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic);
             label1.ForeColor = Color.WhiteSmoke;
             label1.Location = new Point(36, 7);
             label1.Name = "label1";
@@ -92,11 +92,11 @@ namespace Sauli616_Executor
             button3.FlatAppearance.MouseOverBackColor = Color.FromArgb(70, 70, 70);
             button3.FlatStyle = FlatStyle.Flat;
             button3.ForeColor = Color.WhiteSmoke;
-            button3.Location = new Point(406, 4);
+            button3.Location = new Point(468, 4);
             button3.Name = "button3";
             button3.Size = new Size(22, 21);
             button3.TabIndex = 4;
-            button3.Text = "X";
+            button3.Text = "❌";
             button3.UseVisualStyleBackColor = false;
             button3.Click += button3_Click;
             // 
@@ -108,11 +108,11 @@ namespace Sauli616_Executor
             button4.FlatAppearance.MouseOverBackColor = Color.FromArgb(70, 70, 70);
             button4.FlatStyle = FlatStyle.Flat;
             button4.ForeColor = Color.WhiteSmoke;
-            button4.Location = new Point(378, 4);
+            button4.Location = new Point(412, 4);
             button4.Name = "button4";
             button4.Size = new Size(22, 21);
             button4.TabIndex = 5;
-            button4.Text = "-";
+            button4.Text = "━";
             button4.UseVisualStyleBackColor = false;
             button4.Click += button4_Click;
             // 
@@ -138,7 +138,7 @@ namespace Sauli616_Executor
             button6.FlatStyle = FlatStyle.Flat;
             button6.Font = new Font("Segoe UI Emoji", 10F);
             button6.ForeColor = Color.WhiteSmoke;
-            button6.Location = new Point(12, 181);
+            button6.Location = new Point(12, 217);
             button6.Name = "button6";
             button6.Size = new Size(23, 23);
             button6.TabIndex = 8;
@@ -155,11 +155,11 @@ namespace Sauli616_Executor
             button7.FlatStyle = FlatStyle.Flat;
             button7.Font = new Font("Segoe UI Emoji", 10F);
             button7.ForeColor = Color.WhiteSmoke;
-            button7.Location = new Point(42, 181);
+            button7.Location = new Point(41, 218);
             button7.Name = "button7";
             button7.Size = new Size(23, 23);
             button7.TabIndex = 9;
-            button7.Text = "🗃️";
+            button7.Text = "📁";
             button7.UseVisualStyleBackColor = false;
             button7.Click += button7_Click;
             // 
@@ -172,7 +172,7 @@ namespace Sauli616_Executor
             button8.FlatStyle = FlatStyle.Flat;
             button8.Font = new Font("Segoe UI", 8F, FontStyle.Bold);
             button8.ForeColor = Color.WhiteSmoke;
-            button8.Location = new Point(71, 181);
+            button8.Location = new Point(70, 219);
             button8.Name = "button8";
             button8.Size = new Size(27, 23);
             button8.TabIndex = 10;
@@ -180,12 +180,29 @@ namespace Sauli616_Executor
             button8.UseVisualStyleBackColor = false;
             button8.Click += button8_Click;
             // 
+            // button9
+            // 
+            button9.BackColor = Color.FromArgb(28, 28, 28);
+            button9.FlatAppearance.BorderSize = 0;
+            button9.FlatAppearance.MouseDownBackColor = Color.FromArgb(80, 80, 80);
+            button9.FlatAppearance.MouseOverBackColor = Color.FromArgb(70, 70, 70);
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.ForeColor = Color.WhiteSmoke;
+            button9.Location = new Point(440, 4);
+            button9.Name = "button9";
+            button9.Size = new Size(22, 21);
+            button9.TabIndex = 11;
+            button9.Text = "☐";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button9_Click;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
             BackColor = Color.FromArgb(24, 24, 24);
-            ClientSize = new Size(431, 216);
+            ClientSize = new Size(502, 250);
+            Controls.Add(button9);
             Controls.Add(button8);
             Controls.Add(button6);
             Controls.Add(button7);
@@ -219,5 +236,6 @@ namespace Sauli616_Executor
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
     }
 }
